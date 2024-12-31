e2ff3a88f3ea3b7cd9253fea2c1b1bed7d611c13
Jewels <jewels286@proton.me>
2024-12-31 15:11:50 -0500

    tokenizer completely works

--- TestLanguage/Program.cs
+++ TestLanguage/Program.cs
@@ -16,10 +16,19 @@ namespace TestLanguage
 			StandardTokenizer tokenizer = new StandardTokenizer();
 
 			Ruleset ruleset = new Ruleset(";", "//");
-			ruleset.AddTypes(["int"]);
-			ruleset.AddTypeLiteral("int", @"[0-9]+");
-			ruleset.AddTypeLiteral("bool", @"true|false");
+			ruleset.AddTypes(["int", "float", "string", "char", "bool", "void"]);
+			ruleset.AddTypeLiteral("int", TokenizationFunctions.StandardIntegerLiteralRegex);
+			ruleset.AddTypeLiteral("float", TokenizationFunctions.StandardFloatLiteralRegex);
+			ruleset.AddTypeLiteral("string", TokenizationFunctions.StandardStringLiteralRegex);
+			ruleset.AddTypeLiteral("char", TokenizationFunctions.StandardCharLiteralRegex);
+			ruleset.AddTypeLiteral("bool", TokenizationFunctions.StandardBoolLiteralRegex);
+			ruleset.AddTypeLiteral("void", "void");
+			ruleset.AddOperators(TokenizationFunctions.StandardMathOperators);
+			ruleset.AddOperators(TokenizationFunctions.StandardComparisonOperators);
+			ruleset.AddOperator("=");
+			ruleset.SetIdentifier(TokenizationFunctions.StandardIdentifierRegex);
 			tokenizer.AddRules(ruleset.Unpack());
+			tokenizer.AddRule(TokenizationFunctions.CreateSpaceRule());
 
 			foreach (Token token in tokenizer.Tokenize(content))
 			{
