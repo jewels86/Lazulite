81d94f4e72ef402207103323cd299ba29912a78a
Jewels <jewels286@proton.me>
2025-01-02 21:44:43 -0500

    stupid stuff

---
+++ Lazulite/Parsing/GrammarRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazulite.Parsing
+{
+	public delegate List<IAstNode> GrammarRuleActionDelgate(IAstNode node);
+	public class GrammarRule
+	{
+		public List<string> Symbols { get; }
+		public GrammarRuleActionDelgate? Action { get; }
+		
+		public GrammarRule(List<string> symbols, GrammarRuleActionDelgate? action = null)
+		{
+			Symbols = symbols;
+			Action = action;
+		}
+	}
+}
