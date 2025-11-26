d2fb8983c3adafeaa19a093218ee85394204dbcb
Jewels <jewels286@proton.me>
2025-11-26 17:03:58 -0500

    feat: IValueProxy and better IValue (1.0.15)

--- Jewels.Lazulite/Value.cs
+++ Jewels.Lazulite/Value.cs
@@ -1,16 +1,25 @@
-﻿using System.Runtime.CompilerServices;
-using ILGPU;
+﻿using ILGPU;
 using ILGPU.Runtime;
 
 namespace Jewels.Lazulite;
 
-public interface IValue : IDisposable
+public interface IValue : IDisposable 
 {
+    // when preforming elementwise operations, you don't need the host type, just the buffer
+    // IValue lets you do that without needing to know the host type in all cases
     public MemoryBuffer1D<float, Stride1D.Dense> Data { get; }
     public int[] Shape { get; }
     public int TotalSize { get; }
     public int AcceleratorIndex { get; }
     public bool IsValid { get; }
+    
+    // these are used for creating new values from existing ones without knowing the host type
+    // you can safely cast these to Value<T>s- Value<T> is the only class that implements IValue and it will always return the correct type
+    // if for some reason they can't be casted, the object is not a Value<T>
+    public IValue Zeros();
+    public IValue CreateAlike(MemoryBuffer1D<float, Stride1D.Dense> buffer);
+    public IValue Create(MemoryBuffer1D<float, Stride1D.Dense> buffer, int[] shape);
+    public void UpdateWith(IValue other);
 }
 
 public abstract class Value<T>(MemoryBuffer1D<float, Stride1D.Dense> data, int[] shape) : IValue
@@ -41,7 +50,7 @@ public abstract class Value<T>(MemoryBuffer1D<float, Stride1D.Dense> data, int[]
     }
 
     public Value<T> Zeros() => Create(Compute.GetLike(this), Shape);
-    public Value<T> CreateLike(MemoryBuffer1D<float, Stride1D.Dense> buffer) => Create(buffer, Shape);
+    public Value<T> CreateAlike(MemoryBuffer1D<float, Stride1D.Dense> buffer) => Create(buffer, Shape);
 
     public abstract T Unroll(float[] rolled);
     public abstract float[] Roll(T value);
@@ -52,9 +61,25 @@ public abstract class Value<T>(MemoryBuffer1D<float, Stride1D.Dense> data, int[]
     public static implicit operator MemoryBuffer1D<float, Stride1D.Dense>(Value<T> value) => value.Data;
     public static implicit operator ValueProxy<T>(Value<T> value) => value.ToProxy();
     public static implicit operator ArrayView1D<float, Stride1D.Dense>(Value<T> value) => value.Data.View;
+    
+    IValue IValue.Zeros() => Zeros();
+    IValue IValue.CreateAlike(MemoryBuffer1D<float, Stride1D.Dense> buffer) => CreateAlike(buffer);
+    IValue IValue.Create(MemoryBuffer1D<float, Stride1D.Dense> buffer, int[] shape) => Create(buffer, shape);
+    void IValue.UpdateWith(IValue other) => UpdateWith((Value<T>)other);
 }
 
-public abstract class ValueProxy<T>(float[] flatData, int[] shape)
+public interface IValueProxy
+{
+    // these are used for the same purpose as IValue- they let you get a proxy without knowing the host type
+    public float[] FlatData { get; }
+    public int[] Shape { get; }
+    
+    // these should not be used unless you are confident in the host type
+    public float Get(int[] index);
+    public object ToHost();
+}
+
+public abstract class ValueProxy<T>(float[] flatData, int[] shape) : IValueProxy
     where T : notnull
 {
     public float[] FlatData { get; } = flatData;
@@ -64,4 +89,6 @@ public abstract class ValueProxy<T>(float[] flatData, int[] shape)
 
     public abstract float Get(int[] index);
     public abstract T ToHost();
+    
+    object IValueProxy.ToHost() => ToHost();
 }
